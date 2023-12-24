705795d106388870f38e468735f16e928d7be5a9
Aditya Kumar <aryan68125@gmail.com>
2023-12-24 15:12:30 +0530

    how to create and install multiple application in django

--- basic_demo_of_django/basic_demo/course/views.py
+++ basic_demo_of_django/basic_demo/course/views.py
@@ -1,5 +1,38 @@
 from django.shortcuts import render, redirect
 
 # Create your views here.
+'''
+Vikraal 442
+Rollex 997
+Ballistic 117
+Bullet 897
+Barricade 4090
+Barbatos 007
+Endevour 380
+Fury 787
+Leo 747
+LockDown 887
+Dynamite 101
+ShockWave 999
+Blast 036
+Ghost 002
+Nightmare 597
+Dispare 551
+Hurricane 451
+Cyclone 447
+Xcalibur 1000
+Saber 1007
+SilverArrow 1017
+Trident 990
+Obsidian 001
+Venom 097
+Cobra 077
+RedAlert 1080
+Almight 3090
+MahaKaal 051
+Kailash 107
+SideSwipe 111
+Snipe 100
+'''
 def home(request):
     return render(request,'home.html')
