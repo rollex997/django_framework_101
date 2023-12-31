967d6a569060a68083498317a6bdaed1958618e9
Aditya Kumar <aryan68125@gmail.com>
2023-12-31 16:26:56 +0530

    how to show data in a table using template for loop in django

--- basic_demo_of_django/static_templates_practice/course/views.py
+++ basic_demo_of_django/static_templates_practice/course/views.py
@@ -2,5 +2,12 @@ from django.shortcuts import render
 
 # Create your views here.
 def course(request):
-    h1_name = {'page_name' : 'course'}
-    return render(request,"course/course.html",context=h1_name)
+    students = {
+        'stu1' : {'name' : 'Rollex' , 'roll_no' : 997},
+        'stu2' : {'name' : 'Leo' , 'roll_no' : 967},
+        'stu2' : {'name' : 'Ballistic' , 'roll_no' : 747},
+        'stu3' : {'name' : 'Bullet', 'roll_no' : 380},
+        'stu4' : {'name' : 'Barbatos' , 'roll_no' : 1090}
+    }
+    data = {'data' : students}
+    return render(request,"course/course.html",context=data)
