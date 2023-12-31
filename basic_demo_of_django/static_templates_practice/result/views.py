7e6c3937ae5fa25d8d0e785904103f7deafaeb57
Aditya Kumar <aryan68125@gmail.com>
2023-12-31 15:01:19 +0530

    if else tag

--- basic_demo_of_django/static_templates_practice/result/views.py
+++ basic_demo_of_django/static_templates_practice/result/views.py
@@ -3,5 +3,10 @@ from datetime import datetime
 # Create your views here.
 def result(request):
     dt = datetime.now()
-    dictonary = {'dt' : dt}
+    p1 = 56.24321
+    p2 = 56.0000
+    p3 = 56.37000
+    p4 = 59.69999
+    # if tag with 
+    dictonary = {'dt' : dt, 'p1' : p1 , 'p2' : p2 , 'p3' : p3, 'p4' : p4}
     return render(request,"result/result.html",dictonary)
