0466eb8a89fa655b8ee305e58a422a1119292737
Aditya Kumar <aryan68125@gmail.com>
2024-02-18 08:19:25 +0530

    CRUD using SessionAuthentication and (IsAuthenticated, IsAuthenticatedOrReadOnly,DjangoModelPermissions) permission

--- Django_REST_Framework/SessionAuth/auth_api/views.py
+++ Django_REST_Framework/SessionAuth/auth_api/views.py
@@ -3,9 +3,9 @@ from auth_api.models import Student
 from auth_api.serializer import *
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.authentication import SessionAuthentication
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly,DjangoModelPermissions
 class StudentModelViewSet(viewsets.ModelViewSet):
     queryset=Student.objects.all()
     serializer_class=StudentSerializer
     authentication_classes = [SessionAuthentication]
-    permission_classes = [IsAuthenticated]
+    permission_classes = [DjangoModelPermissions]
