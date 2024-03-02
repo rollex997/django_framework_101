d6f939c7047ab6f82034fbb36955a7474efd1801
aditya <aryan68125@gmail.com>
2024-03-02 16:01:25 +0530

    mac commit

--- basic_demo_of_django/django_dynamic_url/django_dynamic_url/settings.py
+++ basic_demo_of_django/django_dynamic_url/django_dynamic_url/settings.py
@@ -17,7 +17,6 @@ from decouple import config
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATE_DIR = BASE_DIR/'templates'
 STATIC_DIR = BASE_DIR/'static'
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
