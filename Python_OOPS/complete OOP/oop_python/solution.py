8c1e3de682857ed174b93096d9a2eed20e0aa389
Aditya Kumar <aryan68125@gmail.com>
2024-04-27 14:03:47 +0530

    polymorphism

--- Python_OOPS/complete OOP/oop_python/solution.py
+++ Python_OOPS/complete OOP/oop_python/solution.py
@@ -1,5 +1,5 @@
 # creating a class
-class Phone:
+class Phone1:
     def __init__(self,brand,model,price,quantity):
         self.brand = brand
         self.model = model
@@ -14,16 +14,16 @@ class Phone:
         return added_total_price_to_gst
 #creating class instance
 print("class Phone related stuff : ")
-myphone = Phone("Motorolla","Razor",1500,3)
-print(f"myphone brand = {myphone.brand}, myphone model = {myphone.model}")
-print(f"myphone price = {myphone.price}, myphone quantity = {myphone.quantity}")
+myphone1 = Phone1("Motorolla","Razor",1500,3)
+print(f"myphone brand = {myphone1.brand}, myphone model = {myphone1.model}")
+print(f"myphone price = {myphone1.price}, myphone quantity = {myphone1.quantity}")
 #calcualte total price
-print(f" myphone calculate total price = {myphone.calulate_total_price()}")
-print(f" myphone calculate gst price = {myphone.calculate_gst()}")
+print(f" myphone calculate total price = {myphone1.calulate_total_price()}")
+print(f" myphone calculate gst price = {myphone1.calculate_gst()}")
 print("\n")
 
 #inheritance
-class SmartPhone(Phone):
+class SmartPhone1(Phone1):
     def __init__(self,brand,model,price,quantity,processor,ram,screen,battery):
         #using this super() we are accessing __init__ in the parent class so that we can insert values using constructor in the parent class
         super().__init__(brand,model,price,quantity)
@@ -33,11 +33,11 @@ class SmartPhone(Phone):
         self.battery = battery
 # creating a class instance
 print("class SmartPhone related stuff : (Inheritance)")
-sp = SmartPhone("Samsung","GALAXY S24 Ultra",124000,1,"snap dragon 8 gen3", "16GB LPDDR4","6.7inch","2600mAH")
-print(f"sp brand = {sp.brand}, sp model = {sp.model}, sp processor = {sp.processor},")
-print(f"sp ram = {sp.ram}, sp screen = {sp.screen}, sp battery = {sp.battery}")
-print(f"sp price = {sp.price}, sp quantity = {sp.quantity}")
-print(f"sp calculate total price = {sp.calulate_total_price()}, sp calculate gst price = {sp.calculate_gst()}")
+sp1 = SmartPhone1("Samsung","GALAXY S24 Ultra",124000,1,"snap dragon 8 gen3", "16GB LPDDR4","6.7inch","2600mAH")
+print(f"sp brand = {sp1.brand}, sp model = {sp1.model}, sp processor = {sp1.processor},")
+print(f"sp ram = {sp1.ram}, sp screen = {sp1.screen}, sp battery = {sp1.battery}")
+print(f"sp price = {sp1.price}, sp quantity = {sp1.quantity}")
+print(f"sp calculate total price = {sp1.calulate_total_price()}, sp calculate gst price = {sp1.calculate_gst()}")
 print("\n")
 
 #encapsulation
@@ -55,7 +55,7 @@ example to get a private attribute and print it in the terminal
 myphone = Phone("Motorolla","Razor",1500,3)
 print(f"myphone brand = {myphone.get_brand()}, myphone model = {myphone.model}")
 '''
-class Phone:
+class Phone2:
     def __init__(self,brand,model,price,quantity):
         self.__brand = brand
         self.__model = model
@@ -89,25 +89,98 @@ class Phone:
         added_total_price_to_gst = total_price + gst
         return added_total_price_to_gst
 print("class Phone related stuff : (Encapsulation)")
-myphone = Phone("Motorolla","Razor",1500,3)
+myphone2 = Phone2("Motorolla","Razor",1500,3)
 '''
 If you change myphone.get_model() to myphone.model then you will get AttributeError: 'Phone' object has no attribute 'model'
 '''
-print(f"myphone brand = {myphone.get_brand()}, myphone model = {myphone.get_model()}")
+print(f"myphone brand = {myphone2.get_brand()}, myphone model = {myphone2.get_model()}")
 print(f"-->trying to change brand using myphone.brand = 'Readme' <--")
-myphone.brand = "lenovo"
-print(f"myphone brand = {myphone.get_brand()}, myphone model = {myphone.get_model()}")
+myphone2.brand = "lenovo"
+print(f"myphone brand = {myphone2.get_brand()}, myphone model = {myphone2.get_model()}")
 print(f"-->trying to change brand using myphone.__brand = 'Readme' <--")
-myphone.__brand = "lenovo"
-print(f"myphone brand = {myphone.get_brand()}, myphone model = {myphone.get_model()}")
+myphone2.__brand = "lenovo"
+print(f"myphone brand = {myphone2.get_brand()}, myphone model = {myphone2.get_model()}")
 print(f"-->trying to change brand using myphone.set_brand('Readme') <--")
-myphone.set_brand("Readme")
-myphone.set_model("Note10")
-myphone.set_price(15000)
-myphone.set_quantity(4)
-print(f"myphone brand = {myphone.get_brand()}, myphone model = {myphone.get_model()}")
-print(f"myphone price = {myphone.get_price()}, myphone quantity = {myphone.get_quantity()}")
+myphone2.set_brand("Readme")
+myphone2.set_model("Note10")
+myphone2.set_price(15000)
+myphone2.set_quantity(4)
+print(f"myphone brand = {myphone2.get_brand()}, myphone model = {myphone2.get_model()}")
+print(f"myphone price = {myphone2.get_price()}, myphone quantity = {myphone2.get_quantity()}")
 #calcualte total price
-print(f"myphone calculate total price = {myphone.calulate_total_price()}")
-print(f"myphone calculate gst price = {myphone.calculate_gst()}")
+print(f"myphone calculate total price = {myphone2.calulate_total_price()}")
+print(f"myphone calculate gst price = {myphone2.calculate_gst()}")
+print("\n")
+
+# polymorphism
+'''
+polymorphism means ek class kitne roop le sakta hai
+'''
+class Iphone(Phone2):
+    def __init__(self,brand,model,price,quantity):
+        super().__init__(brand,model,price,quantity)
+    def os_type(self):
+        return "ios"
+class AndroidPhone(Phone2):
+    def __init__(self,brand,model,price,quantity):
+        super().__init__(brand,model,price,quantity)
+    def os_type(self):
+        return "android"
+print("class iPhone related stuff : (Polymorphism)")
+'''
+you insert data for the first time in a class 
+iphone = Iphone("Apple","14 pro max",174000,4)
+'''
+iphone = Iphone("Apple","14 pro max",174000,4)
+'''
+if you want to make changes in your data after the first insert then you use the method below
+iphone.set_brand("Apple")
+'''
+print(f"IPhone brand = {iphone.get_brand()}, IPhone model = {iphone.get_model()}, IPhone os_type = {iphone.os_type()}")
+print(f"IPhone price = {iphone.get_price()}, IPhone quantity = {iphone.get_quantity()}")
+print(f"IPhone total price = {iphone.calulate_total_price()}, IPhone gst = {iphone.calculate_gst()}")
+print("class AndroidPhone related stuff : (Polymorphism)")
+android = AndroidPhone("Nothing","Phone2",51000,4)
+print(f"android brand = {android.get_brand()}, android model = {android.get_model()}, android os_type = {android.os_type()}")
+print(f"android price = {android.get_price()}, android quantity = {android.get_quantity()}")
+print(f"android total price = {android.calulate_total_price()}, android gst = {android.calculate_gst()}")
+print("\n")
+'''
+A better way to implement polymorphism
+'''
+class SmartPhone2(Phone2):
+    def __init__(self,brand,model,price,quantity,processor,ram,screen):
+        super().__init__(brand,model,price,quantity)
+        self.__processor = processor
+        self.__ram = ram
+        self.__screen = screen
+    def get_processor(self):
+        return self.__processor
+    def set_processor(self,processor):
+        self.__processor = processor
+    
+    def get_ram(self):
+        return self.__ram
+    def set_ram(self,ram):
+        self.__ram = ram
+    
+    def get_screen(self):
+        return self.__screen
+    def set_screen(self,screen):
+        self.__screen = screen
+    
+    def os_type(self):
+        if self.get_brand() == 'Apple' or self.get_brand() == "APPLE" or self.get_brand() == "apple":
+            return "ios"
+        else:
+            return "android"
+print('a better way to handle polymorphism :')
+sp2 = SmartPhone2("Nothing","Phone2",51000,4,"quancomm 8th gen2","12GB","6.1 inch")
+print(f"sp2 brand = {sp2.get_brand()}, sp2 model = {sp2.get_model()}, sp2 os_type = {sp2.os_type()}")
+print(f"sp2 price = {sp2.get_price()}, sp2 quantity = {sp2.get_quantity()}")
+print(f"sp2 total price = {sp2.calulate_total_price()}, sp2 gst = {sp2.calculate_gst()}")
+sp3 = SmartPhone2("Apple","14 pro max",151000,4,"Apple A14","8GB","5.8 inch")
+print(f"sp3 brand = {sp3.get_brand()}, sp3 model = {sp3.get_model()}, sp3 os_type = {sp3.os_type()}")
+print(f"sp3 price = {sp3.get_price()}, sp3 quantity = {sp3.get_quantity()}")
+print(f"sp3 total price = {sp3.calulate_total_price()}, sp3 gst = {sp3.calculate_gst()}")
 print("\n")
